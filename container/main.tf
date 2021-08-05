0cab566a35f8069b57afd54ed2ba2a320c12d327
faithful1 <faithfulanere@gmail.com>
2021-08-06 00:23:28 +0800

    nested modules as volumes

--- container/main.tf
+++ container/main.tf
@@ -1,4 +1,3 @@
-# generate random string
 resource "random_string" "random" {
   count   = var.count_in
   length  = 4
@@ -7,49 +6,32 @@ resource "random_string" "random" {
 }
 
 resource "docker_container" "app_container" {
-  count = length(var.volumes_in)
+  count = var.count_in
   name  = join("-", [var.name_in, terraform.workspace, random_string.random[count.index].result])
   image = var.image_in
-
   ports {
     internal = var.int_port_in
     external = var.ext_port_in[count.index]
   }
-
   dynamic "volumes" {
     for_each = var.volumes_in
     content {
       container_path = volumes.value["container_path_each"]
-      volume_name    = docker_volume.container_volume[volumes.key].name
+      volume_name    = module.volume[count.index].volume_output[volumes.key]
     }
   }
-
-   provisioner "local-exec" {
-    command = "echo ${self.name}: ${self.ip_address}:${join("", [for x in self.ports[*]["external"] : x])} >> containers.txt"
+  provisioner "local-exec" {
+    command = "echo ${self.name}: ${self.ip_address}:${join("", [for x in self.ports[*]["external"] : x])} >> test.txt"
   }
   provisioner "local-exec" {
     when    = destroy
-    command = "rm -f containers.txt"
+    command = "rm -f test.txt"
   }
 }
 
-resource "docker_volume" "container_volume" {
-  count = var.count_in
-  name  = "${var.name_in}-${random_string.random[count.index].result}volume"
-  # lifecycle {
-  #   prevent_destroy = true
-  # }
-
-  // create a backup folder for the volume after destroy
-  provisioner "local-exec" {
-    when = destroy
-    command = "mkdir ${path.cwd}/../backup/"
-    on_failure = continue
-  }
-
-  provisioner "local-exec" {
-    when = destroy
-    command = "sudo tar -czvf ${path.cwd}/../backup/${self.name}.tar.gz ${self.mountpoint}/"
-    on_failure = continue
-  }
+module "volume" {
+  source       = "./volume"
+  count        = var.count_in
+  volume_count = length(var.volumes_in)
+  volume_name  = "${var.name_in}-${terraform.workspace}-${random_string.random[count.index].result}-volume"
 }
--- container/variables.tf
+++ container/variables.tf
@@ -1,28 +1,7 @@
-variable "name_in" {
-  description = "name of image"
-}
-
-variable "image_in" {
-  description = "name of image"
-}
-
-variable "int_port_in" {
-  description = "name of image"
-}
-
-variable "ext_port_in" {
-  description = "name of image"
-}
-
-# variable "container_path_in" {
-#   description = "name of image"
-# }
-
-variable "count_in" {
-  description = "name of count_in"
-
-}
-
-variable "volumes_in" {
-  description = "name of image"
-}
+  
+variable "ext_port_in" {}
+variable "int_port_in" {}
+variable "volumes_in" {}
+variable "image_in" {}
+variable "name_in" {}
+variable "count_in" {}

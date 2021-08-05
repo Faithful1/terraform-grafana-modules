ee6d6c97f9e8e9c1405df25f6eada366ed39a52d
faithful1 <faithfulanere@gmail.com>
2021-08-05 12:59:33 +0800

    container output naming

--- container/outputs.tf
+++ container/outputs.tf
@@ -4,7 +4,7 @@ output "container-name" {
   description = "The name of the nodered container"
 }
 
-output "node-red-ip-address" {
+output "container-ip-address" {
   value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
   description = "The ip address of the container"
 }

c1de60d0d0817231fe1a5b18cf8e779c001c8e2c
faithful1 <faithfulanere@gmail.com>
2021-08-05 23:29:20 +0800

    new outputs and map transformations

--- container/outputs.tf
+++ container/outputs.tf
@@ -1,10 +1,14 @@
 # # output the IP address of the container
 # output "container-ip-address" {
-#   value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
+#   value       = [for i in docker_container.app_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
 #   description = "The ip address of the container"
 # }
 
 # output "container-name" {
-#   value       = docker_container.nodered_container[*].name
+#   value       = docker_container.app_container[*].name
 #   description = "The name of the nodered container"
 # }
+
+output "application_access" {
+    value = { for x in docker_container.app_container[*]: x.name => join(":", [x.ip_address], x.ports[*]["external"])}
+}
--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,7 @@
 # }
 
 
+output "application_access" {
+    value = [for x in module.container[*]: x]
+    description = "The name and socket for each application."
+}

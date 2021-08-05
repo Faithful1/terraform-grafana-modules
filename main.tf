a0f5682d507579b93114f70f8914a940ae2f1f07
faithful1 <faithfulanere@gmail.com>
2021-08-05 14:15:35 +0800

    remove unused host path

--- main.tf
+++ main.tf
@@ -19,5 +19,4 @@ module "container" {
     int_port_in = var.int_port
     ext_port_in = var.ext_port[terraform.workspace][count.index]
     container_path_in = "/data"
-    #   host_path_in      = "${path.cwd}/noderedvol"
 }
